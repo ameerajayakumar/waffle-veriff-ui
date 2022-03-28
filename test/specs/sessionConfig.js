30a71ec05938e957894a5edafca01c8a81d7e432
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-03-28 22:59:48 +0530

    first test pass

--- test/specs/sessionConfig.js
+++ test/specs/sessionConfig.js
@@ -1,7 +1,15 @@
-import {expect} from chai;
+import SessionPage from '../pages/session.page';
+import { expect } from 'chai';
 
-describe('', () => {
-    it('Open URL & verify "demo.saas" is present ', () => {
-        
+describe('Veriff Demo Application', () => {
+  describe('Session Configuration Page', () => {
+    before(() => {
+      SessionPage.open();
     });
+
+    it('Open URL & verify "demo.saas" is present ', async () => {
+      let url = await browser.getUrl();
+      expect(url).to.include('demo.saas');
+    });
+  });
 });
