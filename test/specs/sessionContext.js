beaeaaed11b0e48c40e9d5e187fdcdb1bd0935fd
Ameera Jayakumar <ameeraj.mec@gmail.com>
2022-04-03 16:01:42 +0530

    corrections done for execution

--- test/specs/sessionContext.js
+++ test/specs/sessionContext.js
@@ -20,16 +20,20 @@ describe('Session context flow', () => {
   });
 
   it('Create session in context flow', async () => {
-    //  await SessionPage.fullName.addValue(resources.newName);
     await SessionPage.language.click();
     await SessionPage.languageSelection.click();
+    await waitAndClick(SessionPage.fullName, 10000);
+    await browser.execute((s) => {
+      s.value = null;
+    }, await SessionPage.fullName); //clear existing fullname
+    await SessionPage.fullName.addValue(resources.newName); //add new name
     await SessionPage.country.click();
     await SessionPage.countrySelection.click();
     await SessionPage.documentType.click();
     await SessionPage.documentTypeSelection.click();
     await expect(SessionPage.contextRadio).toBeSelected();
     await SessionPage.veriffMeButton.click();
-    await browser.pause(100000);
+    await browser.pause(5000);
     //validate redirection to next page
   });
 });
